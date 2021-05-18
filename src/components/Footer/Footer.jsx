2aee925c57f5169fc1c670f984c2ea95ae6040c3
vadilog <84346556+vadilog@users.noreply.github.com>
2021-05-19 00:08:57 +0300

    initial commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -38,13 +38,11 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by{' '}
-          <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
-            Jacobo Martínez
+          © {new Date().getFullYear()} -
+          <a href="https://github.com/vadilog" target="_blank" rel="noopener noreferrer">
+            Vadim Ivanov
           </a>
         </p>
-
-        {isEnabled && <GithubButtons />}
       </Container>
     </footer>
   );
