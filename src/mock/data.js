c3c29b2df171e01444ed17b6f10d2c018f24fefa
edwinork <edwinork@gmail.com>
2021-05-19 00:45:22 +0300

    Change title

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,9 +2,9 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Vadim Ivanov', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  description: 'My portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
